e71e7c83ca5537a105e40cb669f3d6b92da09c3f
gallottm <gallottm@D352-S16>
2024-03-29 16:44:38 +0100

    z

--- lib/states/poll_state.dart
+++ lib/states/poll_state.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
 
+import '../models/poll.dart';
+
 class PollsState extends ChangeNotifier {
   Poll? _currentPoll;
+  String? _token;
 
-  // Méthode pour obtenir l'événement actuel
   Poll? get currentPoll => _currentPoll;
 
-  // Méthode pour définir l'événement actuel
   void setCurrentPoll(Poll? poll) {
     _currentPoll = poll;
-    notifyListeners(); // Notifie les écouteurs que l'état a changé
+    notifyListeners();
   }
 
-  // Ajoutez ici d'autres méthodes nécessaires pour obtenir ou modifier les événements via des appels au serveur
+  setAuthToken(String? token) {
+    _token = token;
+    notifyListeners();
+  }
 }
